d91851bd0c5a685daa1337cb01172d30ac4f8c8d
Sasa Nedic <svetasmak0@gmail.com>
2024-03-09 17:28:04 +0100

    Create Player components

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,7 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          
         </ol>
         Game board
       </div>
---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+const Player = () => {
+    return(
+        <li>
+            <span className="player">
+             <span className="player-name">Player 1</span>
+             <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+    )
+}
+export default Player;
