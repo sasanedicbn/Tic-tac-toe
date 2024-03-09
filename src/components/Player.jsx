b2c125445208ac3a2081584cf9c6f7b1354c7e67
Sasa Nedic <svetasmak0@gmail.com>
2024-03-09 17:51:16 +0100

    Create handleEditClick function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 
 const Player = ({name, symbol}) => {
     const [isEditing, setIsEditing] = useState(false)
+    function handleEditClick () {
+        setIsEditing(true)
+    }
     return(
         <li>
             <span className="player">
              <span className="player-name">{name}</span>
              <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
           </li>
     )
 }
