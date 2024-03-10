760b1ea6b1e15cf79caad641cdd251d6c9710f0f
Sasa Nedic <svetasmak0@gmail.com>
2024-03-10 23:52:43 +0100

    Create onChange

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -12,7 +12,7 @@ const Player = ({name, symbol}) => {
     return(
         <li>
             <span className="player">
-             {isEditing ? <input type="text" required value={name} /> : <span className="player-name">{name}</span>}
+             {isEditing ? <input type="text" required value={playerName} onChange={handleChange}/> : <span className="player-name">{name}</span>}
              <span className="player-symbol">{symbol}</span>
             </span>
           <button onClick={handleEditClick}>Edit</button>
