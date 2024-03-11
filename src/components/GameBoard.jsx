ba80aaec7ec60a4304f181c65d8bc32c64ea50cc
Sasa Nedic <svetasmak0@gmail.com>
2024-03-11 15:28:02 +0100

    Create handleSelectSquare function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,6 +7,15 @@ function GameBoard () {
     ]
     const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
+    function handleSelectSquere(rowIndex, columnIndex){
+        setGameBoard((prevGameBoard) => {
+            console.log(prevGameBoard)
+            const updateBoard = [...prevGameBoard.map((innerArray) => [...innerArray])]
+            console.log(updateBoard)
+            updateBoard[rowIndex][columnIndex] = 'X';
+            return updateBoard
+        } )
+    }
     return(
         <ol id="game-board">
          { gameBoard.map((row, rowIndex) => <li key={rowIndex}> 
