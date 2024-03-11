8f0f02f05e7169730accfbdb79b1670423a89ab6
Sasa Nedic <svetasmak0@gmail.com>
2024-03-11 20:43:21 +0100

    Change code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     setActivePlater((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' )
     setGameTurns((prevTurns) => {
       let currentPlayer = 'X'
-      if(currentPlayer.length > 0 && prevTurns[0].player === 'X'){
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
         currentPlayer = '0'
       }
       const updatedTurns = [
@@ -26,7 +26,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelect={handleSelectSquare} activePlayer={activePlayer}/>
+        <GameBoard onSelect={handleSelectSquare} turns={gameTurns}/>
        </div>
     </main>
   )
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,26 +3,23 @@ const initialGameBoard = [
     [null, null, null],
     [null, null, null],
 ]
-function GameBoard ({onSelect}) {
- 
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard)
+function GameBoard ({onSelect, turns}) {
+  
+    let gameBoard = initialGameBoard;
 
-    // function handleSelectSquere(rowIndex, columnIndex){
-    //     setGameBoard((prevGameBoard) => {
-    //         console.log(prevGameBoard)
-    //         const updateBoard = [...prevGameBoard.map((innerArray) => [...innerArray])]
-    //         console.log(updateBoard)
-    //         updateBoard[rowIndex][columnIndex] = activePlayer;
-    //         return updateBoard
-    //     } )
-    //     onSelect()
-    // }
+    for(const turn of turns){
+        const {square, player} = turn
+        const {row, col} = square
+         gameBoard[row][col] = player
+    }
+
+   
     return(
         <ol id="game-board">
          { gameBoard.map((row, rowIndex) => <li key={rowIndex}> 
              <ol>
                { row.map((playerSelect, columnIndex) => <li key={columnIndex}>
-                <button onClick={onSelect}>{playerSelect}</button> 
+                <button onClick={() => onSelect(rowIndex, columnIndex)}>{playerSelect}</button> 
                 </li> )}
             </ol>
             </li>)}
