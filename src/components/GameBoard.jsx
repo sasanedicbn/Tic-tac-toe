3a7758daeb92043c97c42582fcc65299cd4122d8
Sasa Nedic <svetasmak0@gmail.com>
2024-03-11 13:53:18 +0100

    Display initialGameBoard on UX

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,7 +5,13 @@ function GameBoard () {
         [null, null, null],
     ]
     return(
-        
+        <ol id="game-board">
+         { initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>{row}
+             <ol>
+               { row.map((playerSelect, columnIndex) => <li key={columnIndex}><button>{playerSelect}</button> </li> )}
+            </ol>
+            </li>)}
+        </ol>
     )
 }
 
