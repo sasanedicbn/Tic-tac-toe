bc0090befd2c66779039d2e4315bb9456166da4b
Sasa Nedic <svetasmak0@gmail.com>
2024-03-11 22:51:13 +0100

    Remove state and make function

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import { useState } from "react"
+
+function deriveActivePlayer(gameTurns){
+  console.log(gameTurns)
+  let currentPlayer = 'X'
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = '0'
+  }
+  return currentPlayer
+}
 function App() {
   const [gameTurns, setGameTurns] = useState([])
-  const [activePlayer, setActivePlater] = useState('X')
+  const activePlayer = deriveActivePlayer(gameTurns)
 
   function handleSelectSquare (rowIndex, colIndex) {
-    setActivePlater((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' )
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X'
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = '0'
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns)
+
       const updatedTurns = [
         {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
         ...prevTurns
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,6 @@
 const Log = ({turns}) => {
     return(
      <ol id="log">
-        {console.log(turns)}
        {turns.map((turn) => {
         <li key={`${turn.square.row} ${turn.square.col}`}>
          {turn.player} selected {turn.square.row}, {turn.square.col}
