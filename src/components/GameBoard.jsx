044553c2a35ef62433a1ecf3a0cc774b33477ed8
Sasa Nedic <svetasmak0@gmail.com>
2024-03-11 15:27:33 +0100

    Create useState

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 function GameBoard () {
     const initialGameBoard = [
         [null, null, null],
         [null, null, null],
         [null, null, null],
     ]
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
     return(
         <ol id="game-board">
-         { initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>{row}
+         { gameBoard.map((row, rowIndex) => <li key={rowIndex}> 
              <ol>
-               { row.map((playerSelect, columnIndex) => <li key={columnIndex}><button>{playerSelect}</button> </li> )}
+               { row.map((playerSelect, columnIndex) => <li key={columnIndex}>
+                <button }>{playerSelect}</button> 
+                </li> )}
             </ol>
             </li>)}
         </ol>
